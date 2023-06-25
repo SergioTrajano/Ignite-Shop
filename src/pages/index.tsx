501d40dd6e26965f3b5ee9f59677bd8ade1bea34
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-25 18:33:17 -0300

    feat: :sparkles: Add Home interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import shirt2 from "../assets/Shirt-1.png";
+import shirt1 from "../assets/Shirt.png";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+    return (
+        <HomeContainer>
+            <Product>
+                <Image
+                    src={shirt1}
+                    alt=""
+                    width={520}
+                    height={480}
+                />
+
+                <footer>
+                    <strong>Camiseta 1</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </Product>
+            <Product>
+                <Image
+                    src={shirt2}
+                    alt=""
+                    width={520}
+                    height={480}
+                />
+
+                <footer>
+                    <strong>Camiseta 2</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+            </Product>
+        </HomeContainer>
+    );
 }
