0875799bf8d9c6c0c1779fa9ebedb6f61e1ffd36
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-07-01 02:59:47 -0300

    feat: :sparkles: Add Success page interface and styles

---
+++ src/pages/success.tsx
@@ -0,0 +1,19 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
+export default function Success() {
+    return (
+        <SuccessContainer>
+            <h1>Compra efetuada</h1>
+
+            <ImageContainer></ImageContainer>
+
+            <p>
+                Uhuul, <strong>Diego Fernandes</strong>, sua
+                <strong>Camiseta Beyond the Limits</strong> já está a caminho de sua casa.
+            </p>
+
+            <Link href="/">Voltar ao catálogo</Link>
+        </SuccessContainer>
+    );
+}
--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -1,4 +1,4 @@
-import { styled } from "@stitches/react";
+import { styled } from "..";
 
 export const ProductContainer = styled("main", {
     maxWidth: 1180,
