0967b55fb76b445387e1a55a46127985af2fcda9
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-07-01 02:32:01 -0300

    feat: :lipstick: Add button styles when disabled in Product route

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -67,7 +67,13 @@ export const ProductDetails = styled("div", {
 
         cursor: "pointer",
 
-        "&:hover": {
+        "&:disabled": {
+            opacity: 0.6,
+
+            cursor: "not-allowed",
+        },
+
+        "&:not(:disabled):hover": {
             backgroundColor: "$green300",
         },
     },
